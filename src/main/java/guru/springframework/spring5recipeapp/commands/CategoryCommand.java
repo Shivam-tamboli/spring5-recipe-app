4343461ba4c55ef4c1033a8c435695012b80a736
Shivam <shivamtamboli66@gmail.com>
2025-03-31 12:02:09 +0530

    refactor command object for category

--- src/main/java/guru/springframework/spring5recipeapp/commands/CategoryCommand.java
+++ src/main/java/guru/springframework/spring5recipeapp/commands/CategoryCommand.java
@@ -1,2 +1,13 @@
-package guru.springframework.spring5recipeapp.commands;public class CategoryCommand {
+package guru.springframework.spring5recipeapp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CategoryCommand {
+    private Long id;
+    private String description;
 }
