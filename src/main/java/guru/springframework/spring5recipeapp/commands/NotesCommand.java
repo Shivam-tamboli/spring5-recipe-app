1f9c1e21f89449142aa2e332b9f378d78ca47f93
Shivam <shivamtamboli66@gmail.com>
2025-03-31 12:16:30 +0530

    refactor command object for Notes

--- src/main/java/guru/springframework/spring5recipeapp/commands/NotesCommand.java
+++ src/main/java/guru/springframework/spring5recipeapp/commands/NotesCommand.java
@@ -1,2 +1,17 @@
-package guru.springframework.spring5recipeapp.commands;public class NotesCommand {
+package guru.springframework.spring5recipeapp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotesCommand {
+    private Long id;
+    private String recipeNotes;
+
 }
