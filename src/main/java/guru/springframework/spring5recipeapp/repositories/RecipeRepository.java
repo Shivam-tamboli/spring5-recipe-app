8e83dd23969ac94813baf9e75e44b3000549bcdd
Shivam <shivamtamboli66@gmail.com>
2025-04-14 23:40:00 +0530

    Modified RecipeRepository for better query handling

--- src/main/java/guru/springframework/spring5recipeapp/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/RecipeRepository.java
@@ -3,5 +3,7 @@ package guru.springframework.spring5recipeapp.repositories;
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+//RecipeRepository is a specialized interface that connects the Recipe class to the database,
+// extend gains access to all the public methods and properties of the parent class or interface.
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 }
